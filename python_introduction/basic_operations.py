cb1d9b70d87bdd6279c8656d4651c81e77b75b05
sofi1234-1 <sofoniasalemu15@gmail.com>
2024-06-27 10:07:12 +0300

    basic_operations.py

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,5 +1,3 @@
-Python 3.12.4 (tags/v3.12.4:8e8a4ba, Jun  6 2024, 19:30:16) [MSC v.1940 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
 >>> number1 = 10
 >>> number2 = 5
 >>> sum=number1+number2
