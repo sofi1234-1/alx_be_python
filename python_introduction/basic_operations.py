e237a02911af5d7bee5e0eb9dea66290c8c1d4bd
Sofonias <sofoniasalemu15@gmail.com>
2024-06-27 09:43:24 +0300

    coding

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,13 @@
+Python 3.12.4 (tags/v3.12.4:8e8a4ba, Jun  6 2024, 19:30:16) [MSC v.1940 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> number1=10
+>>> number2=5
+>>> sum=number1+number2
+>>> difference=number1-number2
+>>> product=number1*number2
+>>> print("Addition of", number1, "and", number2, "is", sum)
+Addition of 10 and 5 is 15
+>>> print("Subtraction of", number1, "and", number2, "is", difference)
+Subtraction of 10 and 5 is 5
+>>> print("Multiplication of", number1, "and", number2, "is", product)
+Multiplication of 10 and 5 is 50
