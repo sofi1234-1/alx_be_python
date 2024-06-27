8b218d98ed1c79f4f08c8841789a64fa6a01eee8
sofi1234-1 <sofoniasalemu15@gmail.com>
2024-06-27 10:08:46 +0300

    basic_operations.py

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,8 +1,8 @@
 >>> number1 = 10
 >>> number2 = 5
->>> sum=number1+number2
->>> difference=number1-number2
->>> product=number1*number2
+>>> sum = number1+number2
+>>> difference = number1-number2
+>>> product = number1*number2
 >>> print("Addition of", number1, "and", number2, "is", sum)
 Addition of 10 and 5 is 15
 >>> print("Subtraction of", number1, "and", number2, "is", difference)
