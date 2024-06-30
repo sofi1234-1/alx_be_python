14987ce836a9f4e81c47f1d82353d6a8c4756ceb
sofi1234-1 <sofoniasalemu15@gmail.com>
2024-06-30 18:36:02 +0300

    Update daily_reminder.py

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -2,7 +2,6 @@ task = input("Enter your task: ")
 priority = input("Priority (high/medium/low): ").lower()
 time_bound = input("Is it time-bound? (yes/no): ").lower()
 
-
 match priority:
     case "high":
         reminder = f"'{task}' is a high priority task"
@@ -13,11 +12,8 @@ match priority:
     case _:
         reminder = "Invalid priority level."
 
-
 if time_bound == "yes":
     reminder += " that requires immediate attention today!"
 elif time_bound == "no":
     reminder += ". Consider completing it when you have free time."
-
-
 print(reminder)
