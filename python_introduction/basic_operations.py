52d8a35b2e69c338bf658728681004cb65dccc54
sofi1234-1 <sofoniasalemu15@gmail.com>
2024-06-30 14:05:23 +0300

    basic_operations.py

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,6 +1,6 @@
-sum = number1+number2
-difference = number1-number2
-product = number1*number2
+sum = number1 + number2
+difference = number1 - number2
+product = number1 * number2
 print("Addition of", number1, "and", number2, "is", sum)
 print("Subtraction of", number1, "and", number2, "is", difference)
 print("Multiplication of", number1, "and", number2, "is", product)
