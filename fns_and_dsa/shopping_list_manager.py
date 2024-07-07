c2c60c88b7c6d474920401fbbe2cc14543e6a8a5
sofi1234-1 <sofoniasalemu15@gmail.com>
2024-07-07 20:23:23 +0300

    Add files via upload

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,34 @@
+shopping_list = []
+
+while True:
+    print("\nShopping List Menu:")
+    print("1. Add item")
+    print("2. Remove item")
+    print("3. View list")
+    print("4. Exit")
+
+    choice = input("Enter your choice: ")
+
+    if choice == '1':
+        item = input("Enter the item name: ")
+        shopping_list.append(item)
+        print(f"{item} added to the list.")
+    elif choice == '2':
+        item = input("Enter the item to remove: ")
+        if item in shopping_list:
+            shopping_list.remove(item)
+            print(f"{item} removed from the list.")
+        else:
+            print(f"{item} not found in the list.")
+    elif choice == '3':
+        if shopping_list:
+            print("\nShopping List:")
+            for item in shopping_list:
+                print(item)
+        else:
+            print("Your shopping list is empty.")
+    elif choice == '4':
+        print("Exiting...")
+        break
+    else:
+        print("Invalid choice. Please try again.")
