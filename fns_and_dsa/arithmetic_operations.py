33940a1306c88c6cb641ea5b100e730bf1b8cd61
sofi1234-1 <sofoniasalemu15@gmail.com>
2024-07-07 20:17:41 +0300

    Delete fns_and_dsa/arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++
@@ -1,9 +0,0 @@
-Python 3.12.4 (tags/v3.12.4:8e8a4ba, Jun  6 2024, 19:30:16) [MSC v.1940 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> 
-= RESTART: F:/python junkies/arithmetic_operations.py
-Arithmetic Operations
-Enter the first number: 5
-Enter the second number: 6
-Enter the operation (add, subtract, multiply, divide): add
-Result: 11.0
